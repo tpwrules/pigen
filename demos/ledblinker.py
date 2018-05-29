daf27f596aa24ec19339ddc6e1c317aa6d1cf527
Thomas Watson <twatson52@icloud.com>
2018-05-29 13:43:15 +0200

    rewrite ledblinker in accordance with ideal usage

--- demos/ledblinker.py
+++ demos/ledblinker.py
@@ -1,4 +1,5 @@
 from migen import *
+import pigen
 
 class LEDBlinker(Module):
     def __init__(self):
@@ -7,8 +8,10 @@ class LEDBlinker(Module):
         ###
 
         counter = Signal(26)
-        self.comb += self.led.eq(counter[25])
-        self.sync += counter.eq(counter + 1)
+        @pigen.statement()
+        def led_handler():
+            led is counter[25]
+            counter = counter + 1
 
 if __name__ == "__main__":
     from migen.fhdl.verilog import convert
