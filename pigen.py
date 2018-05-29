b530188deb08b32e04e9797d683c0095524d4e93
Thomas Watson <twatson52@icloud.com>
2018-05-29 14:56:00 +0200

    extract statements from ast

--- pigen.py
+++ pigen.py
@@ -44,8 +44,48 @@ def _translate(input_fn, module, domain, comb_allowed, sync_allowed, fsm=None):
 
     # STEP 1: recover the AST of the input function
     tree = _get_ast(input_fn)
-    print(ast.dump(tree))
-    
+    # get the body of the function as the tree
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef):
+            fn_body = node.body
+            break
+    else:
+        raise TranslationError("could not find function def")
+
+    # STEP 2: derive statements from AST
+
+    def parse_body(body, kind=None):
+        statements = []
+        for stmt in body:
+            if isinstance(stmt, ast.Expr):
+                # miiiight be a combinatorial assign
+                if not isinstance(stmt.value, ast.Compare):
+                    raise TranslationError("bare expr is not Compare")
+                stmt = stmt.value
+                if len(stmt.ops) != 1 or not isinstance(stmt.ops[0], ast.Is):
+                    raise TranslationError("bare expr is not 'is'")
+                # ah, it is
+                statements.append(("comb", stmt.left, stmt.comparators[0]))
+                if kind == "sync" and fsm is None:
+                    raise TranslationError("mixing comb and sync inside if")
+            elif isinstance(stmt, ast.Assign):
+                # an assignment statement, this must be synchronous
+                # extract the dest and src
+                if len(stmt.targets) != 1:
+                    raise TranslationError(
+                        "assignment has {} targets, not supported".format(
+                            len(stmt.targets)))
+                dest = stmt.targets[0]
+                src = stmt.value
+                statements.append(("sync", dest, src))
+                if kind == "comb" and fsm is None:
+                    raise TranslationError("mixing comb and sync inside if")
+        return statements
+
+    statements = parse_body(fn_body)
+        
+    print(statements)
+
 def _get_ast(fn):
     # get the AST for a given function
 
